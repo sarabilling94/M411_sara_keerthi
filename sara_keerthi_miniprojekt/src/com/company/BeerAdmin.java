97324b23c64ae983de1e8b53f5e0fe751d098bea
Kvvrtv <kvvrtv@users.noreply.github.com>
2022-01-20 14:34:35 +0100

    ersten Schritte

---
+++ sara_keerthi_miniprojekt/src/com/company/BeerAdmin.java
@@ -0,0 +1,22 @@
+public class BeerAdmin {
+
+
+
+
+
+    public void loadBeerStyles(){
+
+    }
+
+    public void printBeerStyles(){
+
+    }
+
+    public void printBeerStyles(String search){
+
+    }
+    public void getBeerListForStyle(int idStyle){
+
+    }
+
+}
---
+++ sara_keerthi_miniprojekt/src/com/company/beer.java
@@ -0,0 +1,21 @@
+public class beer {
+    private String id;
+    private String name;
+    private String description;
+    private int idStyle;
+
+    public beer(String id, String name, String description, int idStyle){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.idStyle = idStyle;
+    }
+
+    public void printBeerList(){
+
+    }
+
+    public void printBeer(String id){
+
+    }
+}
