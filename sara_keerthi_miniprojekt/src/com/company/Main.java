df1d3628a2cda81b27f2e67884c849378235f98e
Sara <Sara@LAPTOP-SARA>
2022-01-20 14:27:17 +0100

    added jsonreader

--- sara_keerthi_miniprojekt/src/com/company/Main.java
+++ sara_keerthi_miniprojekt/src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+
+import java.io.IOException;
+import org.json.JSONObject;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        JSONObject json = JsonReader.readJsonFromUrl("http://api.brewerydb.com/v2/beers/?key=1511d0db4a1d6841481c672455358cff&styleId=5");
+        System.out.println(json.toString());
     }
 }
