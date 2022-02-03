8a76f5354360d05ffa3880364a251f71c1c7dc4e
Sara <Sara@LAPTOP-SARA>
2022-02-03 14:34:43 +0100

    load beer

--- sara_keerthi_miniprojekt/src/com/company/BeerAdmin.java
+++ sara_keerthi_miniprojekt/src/com/company/BeerAdmin.java
@@ -10,6 +10,7 @@ import org.json.simple.parser.ParseException;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,10 +18,21 @@ import java.util.Iterator;
 public class BeerAdmin {
     private HashMap<Integer, String> beers;
 
+    public ArrayList<beer> loadBeers() throws IOException {
+        ArrayList<beer> beers = new ArrayList<>();
+        JsonNode jsonNode = JsonReader.apiCall("beers", "");
+        for (JsonNode style : jsonNode.get("data")) {
+            String id = style.get("id").asText();
+            String name = style.get("name").asText();
+            String description = style.get("description").asText();
+            Integer idStyle = style.get("styleId").asInt();
+            beers.add(new beer(id, name, description, idStyle));
+        }
+        return beers;
+    }
+
     public static HashMap<Integer, String> loadBeerStyles() throws IOException {
-        String url = "http://api.brewerydb.com/v2/styles/?key=1511d0db4a1d6841481c672455358cff";
         HashMap hashMap = new HashMap<Integer, String>();
-        JSONObject json = JsonReader.readJsonFromUrl(url);
         JsonNode jsonNode = JsonReader.apiCall("styles", "");
         for (JsonNode style : jsonNode.get("data")) {
             Integer id = style.get("id").asInt();
